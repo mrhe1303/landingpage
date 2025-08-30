05af0fe9374a701ab2b78fe7f655709bf9b41b91
mjfy93 <maria.jose.ferro@hotmail.com>
2025-08-30 18:39:42 -0500

    Navbar and Jumbotron Done and added to main.jsx

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,15 @@
+export function Jumbotron() {
+    return (
+        <div className="jumbotron p-3 m-5 bg-body-secondary">
+        <h1 className="display-4">Welcome to Albuquerque, New Mexico! </h1>
+            <p className="lead">Dive into the world of Walter White and Jesse Pinkman as we explore the gripping series that spans five intense seasons. This acclaimed show delves into themes of morality, crime, power, and consequences, offering a thought-provoking look at the transformation of ordinary individuals into complex characters. Join us as we celebrate the storytelling, characters, and impact of Breaking Bad!</p>
+        
+            <p>Get to know the characters, the amazing cast that brought them to life, get fun facts and anecdotes from the production team, and much, much more.</p>
+            <p className="lead">
+                <a className="btn btn-info btn-lg  " href="#" role="button">Get started</a>
+            </p>
+       
+    </div>
+    );
+
+};
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,15 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import { Navbar } from './components/Navbar';
+import { Jumbotron } from './components/Jumbotron';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+  
+    <Navbar/>
+    <Jumbotron/>
+    
   </React.StrictMode>,
 )
